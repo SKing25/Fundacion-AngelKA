5dfc8d918851bc3c435f095dab38b6d1f1324963
Sking25 <santiagorodri2509@gmail.com>
2025-06-01 14:18:53 -0500

    hosts

--- citas_psicologos/citas_psicologos/settings.py
+++ citas_psicologos/citas_psicologos/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-sj-@tvne7t43u&auf3&&i6k8vdehr^#&r2cjcb0$*xds6u#23e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'fundacion-angelka.onrender.com']
 PORT = 8000
 HOST = '0.0.0.0'
 # Application definition
